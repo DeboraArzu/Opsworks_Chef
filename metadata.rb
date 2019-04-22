424855f5e3166e3ba7a7e086fe1006b91267d06c
deboraarzu <deboranise29@hotmail.com>
2019-04-22 09:20:29 -0600

    chef version updated

--- metadata.rb
+++ metadata.rb
@@ -5,16 +5,4 @@ license 'All Rights Reserved'
 description 'Installs/Configures Redis'
 long_description 'Installs/Configures Redis'
 version '0.1.0'
-chef_version '>= 12.18.31'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/Redis/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/Redis'
