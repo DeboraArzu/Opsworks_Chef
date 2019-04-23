f588c1f2d76c4dd5c8a73824a28d6227e8b73dc9
deboraarzu <deboranise29@hotmail.com>
2019-04-23 15:22:18 -0600

    upate

--- recipes/default.rb
+++ recipes/default.rb
@@ -45,7 +45,7 @@ end
 
 # sudo service redis_6379 start
 service "redis_6379" do
-  action [ :enable, :start ]
+  action [ :start, :enable ]
   # This is necessary so that the service will not keep reporting as updated
-  supports :status => true, :restart => true
+  supports :status => true
 end
